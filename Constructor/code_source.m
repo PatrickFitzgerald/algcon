0037b4783111955a6b9b1e678cbd234ba54582ba
Patrick Fitzgerald <patrick.fitzgerald347@gmail.com>
2020-10-25 18:05:21 -0400

    These are the files I've been using to test out the functionality I'm adding to the AlgorithmConstructor

---
+++ Constructor/code_source.m
@@ -0,0 +1,30 @@
+			
+		% 
+			w.wrapper1 = Wrapper1();
+			
+			v.const1.stuff(23) = 123;
+			
+			w.myWrap2 = w_r_a_p(...
+				125);
+			
+			
+		    v.hwody = nan+'a';
+			
+			cow = v.one.two;
+			v.one.two(3) = v.one.two(3);
+			disp(v.one.two(3));
+			
+			[v.heyyy,qqq] = size(randn(3,4));
+			
+		v.a = v.const1 * 2 + 77.2;
+		q = 99;
+		v.b = q.nothing;
+		v.c = v.a.hello.^2 + w.wrapper1(q);
+			
+
+
+
+
+
+
+		test
